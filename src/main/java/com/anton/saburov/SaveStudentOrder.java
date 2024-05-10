4e5a90945afa752cee95028998270cbb8cdd32d2
Bayakan <spring.boot@hotmail.com>
2024-05-10 14:29:58 +0300

    Нестандартные операции.

--- src/main/java/com/anton/saburov/SaveStudentOrder.java
+++ src/main/java/com/anton/saburov/SaveStudentOrder.java
@@ -3,5 +3,13 @@ package com.anton.saburov;
 public class SaveStudentOrder {
     public static void main(String[] args) {
         System.out.println("SaveStudentOrder is running");
+        saveStudentOrder();
+        saveStudentOrder();
+    }
+
+    static void saveStudentOrder() {
+        System.out.println("saveStudentOrder 1");
+        System.out.println("saveStudentOrder 2");
+        System.out.println("saveStudentOrder 3");
     }
 }
